a61d3d162162eabf5957cc937ca5cdfa43747cde
Sakai Fuka <s2322028@stu.musashino-u.ac.jp>
2024-01-15 20:08:04 +0900

    もう一つの方も取り出せた

--- job.py
+++ job.py
@@ -18,3 +18,28 @@ for i in range(1,17):
         mj_name = mj_tag.text.strip()
         mac_job_list.append(mj_name)
         print(mj_name)
+
+from bs4 import BeautifulSoup
+import requeｓts
+import time
+#リスト作る
+windows_job_list = []
+for i in range(1,71):
+    #time.sleep(1)
+    url = f'https://www.itreview.jp/products/windows-10/reviews?page={i}'
+    r=requests.get(url)
+
+#文字化け少なく（意味なし？）
+    r.encoding = r.apparent_encoding
+#HTMLを解析する
+    html_soup = BeautifulSoup(r.text, "html.parser")
+    #文字化け少なく（意味なし？）
+    r.encoding = r.apparent_encoding
+#HTMLを解析する
+    html_soup = BeautifulSoup(r.text, "html.parser")
+#レビュースター名
+    wj_span_list = html_soup.find_all(class_="product-review-info")
+    for wj_tag in wj_span_list:
+        wj_name = wj_tag.text.strip()
+        windows_job_list.append(wj_name)
+        print(wj_name)
